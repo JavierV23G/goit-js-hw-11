36078e826af8dbdbead12957696d77369bf805a7
JavierV23G <127901386+JavierV23G@users.noreply.github.com>
2023-11-29 12:14:37 -0500

    Almost finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,98 @@
 import axios from 'axios';
-const ApiKey = "40960381-08cae8203cb7081d274a7ee8e";
+import Notiflix from 'notiflix';
 
-    axios.get(`https://pixabay.com/api/?key=${ApiKey}`)
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery__images');
+const loadMore = document.querySelector('.load__images');
 
+async function getApiInfo(searched, pages) {
+    const url = 'https://pixabay.com/api/'
+    const params= {
+        key: '40960381-08cae8203cb7081d274a7ee8e',
+        q: searched,
+        safeSearch: true,
+        orientation: "horizontal",
+        page: pages
+    }
+    try{
+        const response = await axios.get(url, { params })
+        const images = response.data;
+        return images
+    } catch(err) {
+        console.log("ERROR", err);
+    }
+}
+
+function galleryStructure(data) {
+    const HTMLelements = data.map(element => {
+        return `
+            <div class="cat__card">
+                <img class="cat__img" src="${element.webformatURL}" alt="photo" loading="lazy">
+
+                <ul class="cat__info">
+                    <li class="cat__data">
+                        <h4 class="cat__data__title">Likes</h4>
+                        <p class="cat__data__value">${element.likes}</p>
+                    </li>
+
+                    <li class="cat__data">
+                        <h4 class="cat__data__title">Views</h4> 
+                        <p class="cat__data__value">${element.views}</p>
+                    </li>
+
+                    <li class="cat__data"> 
+                        <h4 class="cat__data__title">Comments</h4>
+                        <p class="cat__data__value">${element.comments}</p>
+                    </li>
+
+                    <li class="cat__data"> 
+                        <h4 class="cat__data__title">Downloads</h4>
+                        <p class="cat__data__value">${element.downloads}</p>
+                    </li>
+                </ul>
+            </div>
+            `;
+    }).join('');
+
+    return HTMLelements;
+}
+
+let search = ''
+let images = [];
+let page = 1;
+
+const searchInfo = async () => {
+    try {
+        const fullInfo = await getApiInfo(search, page);
+        fullInfo.hits.forEach(element => {
+            images.push(element)
+        });
+
+        gallery.innerHTML = galleryStructure(images)
+
+        if(page === 1){
+            if(images.length === 0){
+                Notiflix.Notify.failure('Oh, unfortunately there are no images matching with your search. Try again')
+                loadMore.classList.add('hidden')
+            }else{
+                Notiflix.Notify.success(`Awesome! We found ${fullInfo.totalHits} images.`)
+                loadMore.classList.remove('hidden')
+            }
+        }
+    } catch(err) {
+        console.error('ERROR:', err);
+    }
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    search = form.elements.searchQuery.value;
+    page = 1
+    images = []
+    searchInfo()
+}); 
+
+loadMore.addEventListener('click', async e =>{
+    page += 1;
+    searchInfo()
+});
